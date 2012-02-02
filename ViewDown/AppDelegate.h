00a6464d54796ca365a28302f4c276c4efd153d9
Martin Algesten <martin@algesten.se>
2012-02-02 19:45:15 +0530

    Initial Commit

---
+++ ViewDown/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ViewDown
+//
+//  Created by martin on 02/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
